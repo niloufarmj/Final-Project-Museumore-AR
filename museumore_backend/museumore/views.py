6432051f07ea4f4742c89fbafa457437fa1dc696
Niloufar <niloo.ast@gmail.com>
2022-12-27 21:02:35 +0330

    add gallery (empty function)

--- museumore_backend/museumore/views.py
+++ museumore_backend/museumore/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def add_gallary(request):
+    if request.method == 'POST':
+        # nemidunam
+
--- museumore_backend/museumore_backend/settings.py
+++ museumore_backend/museumore_backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'museumore'
+    'museumore',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000',
+]
