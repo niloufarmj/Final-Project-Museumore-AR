eb907893c3a382c1cca10528ba9c345ad94b2993
kimiasedighi <kimiasedighi2000@gmail.com>
2022-12-27 17:01:12 +0330

    add backend admin

--- museumore_backend/museumore/models.py
+++ museumore_backend/museumore/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Gallary(models.Model):
-    username = models.CharField()
-    password = models.CharField()
-    name = models.CharField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     email = models.EmailField()
     image = models.ImageField()
     address = models.TextField()
@@ -12,9 +12,9 @@ class Gallary(models.Model):
 
 
 class Item(models.Model):
-    title = models.CharField()
-    description = models.CharField()
+    title = models.CharField(max_length=50)
+    description = models.TextField()
     audio = models.FileField(upload_to='audios/')
     video = models.FileField(upload_to='videos/')
     augmented_video = models.FileField()
-    gallary_id = models.CharField()
+    gallary_id = models.IntegerField()
