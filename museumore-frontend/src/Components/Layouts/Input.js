f6e7cb7c7f17bfc575e75fbbd07e700d4efb570f
kimiasedighi <kimiasedighi2000@gmail.com>
2023-02-06 15:03:01 +0330

    fix eye icon

--- museumore-frontend/src/Components/Layouts/Input.js
+++ museumore-frontend/src/Components/Layouts/Input.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../Assets/CSS/input.css";
+import eye_icon from "../../Assets/Media/eye-icon.png";
 
 function Input(params) {
+  const [type, setType] = useState(params.type);
+  const [isEyeVisible, setIsEyeVisible] = useState();
+
+  useEffect(() => {
+    if (params.type == "password") {
+      setIsEyeVisible(true);
+    }
+  }, []);
+
   function handleChange(event) {
     params.stateChanger(event.target.value);
   }
+  function toggleVisibility(event) {
+    if (type == "password") {
+      setType("text");
+    } else {
+      setType("password");
+    }
+  }
 
+  let eye_style = {
+    width: "23px",
+    height: "23px",
+    position:"absolute",
+    marginLeft: "78%",
+    marginTop: "-12%"
+  };
+  
   return (
-    <input
-      type={params.type}
-      placeholder={params.text}
-      value={params.initText}
-      className={"input " + params.border}
-      onChange={handleChange}
-    />
+    <>
+      <input
+        type={type}
+        placeholder={params.text}
+        value={params.initText}
+        className={"input " + params.border}
+        onChange={handleChange}
+      />
+      {isEyeVisible && (
+        <img src={eye_icon} style={eye_style} onClick={toggleVisibility} />
+      )}
+    </>
   );
 }
 
